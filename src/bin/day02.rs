122770e1bd17afafaae556276df88888b42d94b1
Wayne He <28408948+Ecpii@users.noreply.github.com>
2023-12-02 01:31:38 -0500

    day 2 part 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::max,
     fs,
     path::{Path, PathBuf},
 };
@@ -19,21 +20,62 @@ fn get_input_dir() -> PathBuf {
 }
 
 struct CubeCount {
-    red: usize,
-    green: usize,
-    blue: usize,
+    pub red: usize,
+    pub green: usize,
+    pub blue: usize,
+}
+
+impl CubeCount {
+    pub fn from_str(s: &str) -> CubeCount {
+        let mut res = CubeCount {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+        for cube_color in s.split(',') {
+            let mut cube_iter = cube_color.split(' ').skip(1);
+            let quantity = cube_iter
+                .next()
+                .expect("quantity should exist")
+                .parse()
+                .unwrap();
+            match cube_iter.next().unwrap() {
+                "red" => res.red = quantity,
+                "green" => res.green = quantity,
+                "blue" => res.blue = quantity,
+                _ => panic!(),
+            };
+        }
+        res
+    }
+
+    pub fn merge(&self, other: &CubeCount) -> CubeCount {
+        CubeCount {
+            red: (max(self.red, other.red)),
+            green: (max(self.green, other.green)),
+            blue: (max(self.blue, other.blue)),
+        }
+    }
+
+    pub fn get_power(&self) -> usize {
+        self.red * self.green * self.blue
+    }
 }
 
 fn main() {
     let filename = get_input_dir().join("input.txt");
     let contents = fs::read_to_string(filename).expect("opening file failed");
-    let (part1, part2) = solve(contents);
-    println!("{}", part1)
+    let part1 = part1(contents);
+    println!("part 1: {}", part1);
+    let filename = get_input_dir().join("input.txt");
+    let contents = fs::read_to_string(filename).expect("opening file failed");
+    let part2 = part2(contents);
+    println!("part 2: {}", part2)
 }
 
-fn solve(contents: String) -> (u32, u32) {
+fn part1(contents: String) -> u32 {
     let games = contents.split('\n').take_while(|x| !x.is_empty());
-    let mut part1 = 0;
+    let mut result = 0;
     for game in games {
         let colon_index = game.chars().position(|x| x == ':').unwrap();
         let game_num: u32 = game[5..colon_index].parse().unwrap();
@@ -55,56 +97,49 @@ fn solve(contents: String) -> (u32, u32) {
             }
             true
         }) {
-            part1 += game_num
+            result += game_num
         }
-        // game[colon_index + 1..].split([',', ';']).fold(
-        //     CubeCount {
-        //         red: 0,
-        //         green: 0,
-        //         blue: 0,
-        //     },
-        //     |CubeCount { red, green, blue }, string| {
-        //         let mut string_iter = string.split(' ').skip(1);
-        //         let quantity: usize = string_iter
-        //             .next()
-        //             .expect("quantity should exist")
-        //             .parse()
-        //             .unwrap();
-        //         match string_iter.next() {
-        //             Some("red") => CubeCount {
-        //                 red: red + quantity,
-        //                 green,
-        //                 blue,
-        //             },
-        //             Some("green") => CubeCount {
-        //                 green: green + quantity,
-        //                 red,
-        //                 blue,
-        //             },
-        //             Some("blue") => CubeCount {
-        //                 blue: blue + quantity,
-        //                 green,
-        //                 red,
-        //             },
-        //             _ => panic!("second word should be one of red, green, blue"),
-        //         }
-        //     },
-        // );
     }
-    (part1, 0)
+    result
+}
+
+fn part2(contents: String) -> usize {
+    let games = contents.split('\n').take_while(|x| !x.is_empty());
+    let mut result = 0;
+    for game in games {
+        let miniminum_set: CubeCount = game.split(':').nth(1).unwrap().split(';').fold(
+            CubeCount {
+                red: 0,
+                green: 0,
+                blue: 0,
+            },
+            |accumulator, game_round| accumulator.merge(&CubeCount::from_str(game_round)),
+        );
+        result += miniminum_set.get_power()
+    }
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_input_dir, solve};
+    use crate::{get_input_dir, part1, part2};
     use std::fs;
 
     #[test]
-    fn sample_01() {
+    fn p1sample01() {
         let filename = get_input_dir().join("sample.txt");
         let contents = fs::read_to_string(filename).expect("opening file failed");
         println!("{:?}", contents);
-        let (part_1, part_2) = solve(contents);
+        let part_1 = part1(contents);
         assert_eq!(part_1, 8);
+    }
+    #[test]
+    fn p2sample01() {
+        let filename = get_input_dir().join("sample.txt");
+        let contents = fs::read_to_string(filename).expect("opening file failed");
+        println!("{:?}", contents);
+        let part_2 = part2(contents);
+        assert_eq!(part_2, 2286);
     }
 }
