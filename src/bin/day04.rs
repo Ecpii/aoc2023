37a94b66b88758664b3b4ce89cde379e09a70c69
Wayne He <28408948+Ecpii@users.noreply.github.com>
2023-12-04 00:39:29 -0500

    day 4 part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -44,8 +44,48 @@ fn part1(contents: String) -> usize {
 }
 
 fn part2(contents: String) -> usize {
-    contents.split('\n').for_each(|_| ());
-    0
+    let mut total_cards = 0usize;
+    let mut multipliers: Vec<usize> = vec![1];
+    println!("{:?}", multipliers);
+    for (card_index, card) in contents
+        .split('\n')
+        .take_while(|x| !x.is_empty())
+        .enumerate()
+    {
+        let mut number_sections = card.split(": ").nth(1).unwrap().split(" | ");
+        let winning_number_section = number_sections.next().unwrap();
+        let owned_number_section = number_sections.next().unwrap();
+
+        let mut winning_numbers = HashSet::new();
+        winning_number_section.split_whitespace().for_each(|x| {
+            winning_numbers.insert(x);
+        });
+
+        let mut owned_numbers = HashSet::new();
+        owned_number_section.split_whitespace().for_each(|x| {
+            owned_numbers.insert(x);
+        });
+
+        let current_multiplier = *multipliers.get(card_index).unwrap_or(&1);
+
+        let winning_count = winning_numbers
+            .intersection(&owned_numbers)
+            .fold(0usize, |x, _| x + 1);
+        for i in card_index + 1..card_index + winning_count + 1 {
+            match multipliers.get(i).take() {
+                Some(x) => multipliers[i] = x + current_multiplier,
+                None => {
+                    while multipliers.len() < i {
+                        multipliers.push(1);
+                    }
+                    multipliers.push(current_multiplier + 1);
+                }
+            }
+        }
+
+        total_cards += current_multiplier;
+    }
+    total_cards
 }
 
 #[cfg(test)]
@@ -53,7 +93,7 @@ mod tests {
     use crate::{part1, part2, read_input_file};
 
     const P1SAMPLE01_ANSWER: usize = 13;
-    const P2SAMPLE01_ANSWER: usize = 0;
+    const P2SAMPLE01_ANSWER: usize = 30;
 
     #[test]
     fn p1sample01() {
