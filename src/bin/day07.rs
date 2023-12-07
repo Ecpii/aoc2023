b5bec1d9a78f4499b6aef80378fa34d2f489fef0
Wayne He <28408948+Ecpii@users.noreply.github.com>
2023-12-07 00:57:56 -0500

    day 7 part 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -8,7 +8,7 @@ fn main() {
     let part1 = part1(contents); // 241344943
     println!("part 1: {}", part1);
     let contents = read_input_file(file!(), "input.txt");
-    let part2 = part2(contents);
+    let part2 = part2(contents); // NOT 243101568
     println!("part 2: {}", part2)
 }
 
@@ -104,10 +104,95 @@ fn part1(contents: String) -> usize {
         .enumerate()
         .fold(0, |total, (rank, (_, bid))| total + bid * (rank + 1))
 }
+static PART_2_CARD_ORDER: [char; 13] = [
+    'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
+];
 
+fn part_2_get_hand_type(hand: &str) -> HandType {
+    let card_counts = hand.chars().counts();
+    let joker_count = *card_counts.get(&'J').unwrap_or(&0usize);
+    let mut most_freq_count = 0;
+    let mut most_freq_char = 'H';
+    let mut second_most_freq_count = 0;
+    let mut _second_most_freq_char = 'H';
+    for (card, count) in card_counts {
+        if card == 'J' {
+            continue;
+        }
+        if count > most_freq_count {
+            second_most_freq_count = most_freq_count;
+            _second_most_freq_char = most_freq_char;
+            most_freq_count = count;
+            most_freq_char = card;
+        } else if count > second_most_freq_count {
+            _second_most_freq_char = card;
+            second_most_freq_count = count;
+        }
+    }
+    match most_freq_count + joker_count {
+        5 => HandType::FiveKind,
+        4 => HandType::FourKind,
+        3 => {
+            if second_most_freq_count == 2 {
+                HandType::FullHouse
+            } else {
+                HandType::ThreeKind
+            }
+        }
+        2 => {
+            if second_most_freq_count == 2 {
+                HandType::TwoPair
+            } else {
+                HandType::OnePair
+            }
+        }
+        _ => HandType::HighCard,
+    }
+}
 fn part2(contents: String) -> usize {
-    contents.split('\n').for_each(|_| ());
-    0
+    let mut hands_bids: Vec<(&str, usize)> = contents
+        .split('\n')
+        .take_while(|x| !x.is_empty())
+        .map(|x| {
+            let mut parts = x.split(' ');
+            (
+                parts.next().unwrap(),
+                parts.next().map(|x| x.parse::<usize>().unwrap()).unwrap(),
+            )
+        })
+        .collect();
+
+    hands_bids.sort_unstable_by(|(l, _), (r, _)| {
+        let left_hand_type = part_2_get_hand_type(l);
+        let right_hand_type = part_2_get_hand_type(r);
+        if left_hand_type > right_hand_type {
+            Ordering::Less
+        } else if left_hand_type < right_hand_type {
+            Ordering::Greater
+        } else {
+            for (left_card, right_card) in l.chars().zip(r.chars()) {
+                let left_lead_value = PART_2_CARD_ORDER
+                    .iter()
+                    .position(|x| *x == left_card)
+                    .unwrap();
+                let right_lead_value = PART_2_CARD_ORDER
+                    .iter()
+                    .position(|x| *x == right_card)
+                    .unwrap();
+                let compare_result = left_lead_value.cmp(&right_lead_value);
+                if compare_result != Ordering::Equal {
+                    return compare_result;
+                }
+            }
+            Ordering::Equal
+        }
+    });
+
+    println!("{:?}", hands_bids);
+    hands_bids
+        .iter()
+        .enumerate()
+        .fold(0, |total, (rank, (_, bid))| total + bid * (rank + 1))
 }
 
 #[cfg(test)]
@@ -115,7 +200,7 @@ mod tests {
     use crate::{part1, part2, read_input_file};
 
     const P1SAMPLE01_ANSWER: usize = 6440;
-    const P2SAMPLE01_ANSWER: usize = 0;
+    const P2SAMPLE01_ANSWER: usize = 5905;
 
     #[test]
     fn p1sample01() {
