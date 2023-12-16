4abbfb1ca2862d6b7139431612b3d51ce7e4b138
Wayne He <28408948+Ecpii@users.noreply.github.com>
2023-12-15 20:14:47 -0500

    update template

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -22,19 +22,30 @@ fn part2(contents: String) -> isize {
 #[cfg(test)]
 mod tests {
     use crate::{part1, part2, read_input_file};
-
-    const SAMPLE_ANSWERS: [(isize, isize); 1] = [(0, 0)];
-
-    #[test]
-    fn sample01p1() {
-        let contents = read_input_file(file!(), "sample.txt");
-        let res = part1(contents);
-        assert_eq!(res, SAMPLE_ANSWERS[0].0);
+    struct Sample {
+        pub input_file: &'static str,
+        pub part_num: u8,
+        pub expected_out: isize,
     }
+    impl Sample {
+        pub fn run(&self) {
+            let contents = read_input_file(file!(), self.input_file);
+            let res = if self.part_num == 1 {
+                part1(contents)
+            } else {
+                part2(contents)
+            };
+            assert_eq!(res, self.expected_out);
+        }
+    }
+
     #[test]
-    fn sample01p2() {
-        let contents = read_input_file(file!(), "sample.txt");
-        let res = part2(contents);
-        assert_eq!(res, SAMPLE_ANSWERS[0].1);
+    fn first_sample() {
+        Sample {
+            input_file: "sample.txt",
+            part_num: 1,
+            expected_out: 0,
+        }
+        .run()
     }
 }
