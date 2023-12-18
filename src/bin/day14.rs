0ec6bb30819427bd6ae6bfdb0a0f0fe7ce9c4a55
Wayne He <28408948+Ecpii@users.noreply.github.com>
2023-12-17 22:44:16 -0500

    day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc2023::utils::read_input_file;
 use array2d::Array2D;
 
@@ -8,6 +10,14 @@ fn main() {
     let contents = read_input_file(file!(), "input.txt");
     let part2 = part2(contents);
     println!("part 2: {}", part2)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
 }
 
 fn part1(contents: String) -> isize {
@@ -50,8 +60,99 @@ fn part1(contents: String) -> isize {
     res as isize
 }
 
+fn tilt_direction(map: &mut Array2D<char>, direction: Direction) {
+    let width = map.num_columns();
+    let height = map.num_rows();
+    let is_vertical = direction == Direction::North || direction == Direction::South;
+    let is_inverted = direction == Direction::South || direction == Direction::East;
+    for i in 0..(if is_vertical { width } else { height }) {
+        let mut rock_line: Vec<char> = if is_vertical {
+            map.column_iter(i).unwrap().cloned().collect()
+        } else {
+            map.row_iter(i).unwrap().cloned().collect()
+        };
+
+        if is_inverted {
+            rock_line.reverse();
+        }
+        let mut landing_area = 0;
+        for j in 0..(if is_vertical { height } else { width }) {
+            let current_rock = rock_line[j];
+            if current_rock == '#' {
+                landing_area = j + 1
+            } else if current_rock == 'O' {
+                if landing_area < j {
+                    rock_line[j] = '.';
+                    rock_line[landing_area] = 'O';
+                }
+                while rock_line
+                    .get(landing_area)
+                    .is_some_and(|landing_rock| *landing_rock != '.')
+                {
+                    landing_area += 1;
+                }
+            }
+        }
+        if is_inverted {
+            rock_line.reverse();
+        }
+
+        for (j, &new_rock) in rock_line.iter().enumerate() {
+            if is_vertical {
+                map.set(j, i, new_rock).expect("Setting rock line failed");
+            } else {
+                map.set(i, j, new_rock).expect("Setting rock line failed");
+            }
+        }
+    }
+}
+
+fn spin_cycle(map: &mut Array2D<char>) {
+    let directions = [
+        Direction::North,
+        Direction::West,
+        Direction::South,
+        Direction::East,
+    ];
+    for direction in directions {
+        tilt_direction(map, direction);
+    }
+}
+
+fn get_north_load(map: &Array2D<char>) -> usize {
+    let height = map.num_rows();
+    map.enumerate_row_major()
+        .fold(0, |sum, ((y, _), &terrain)| {
+            if terrain == 'O' {
+                sum + height - y
+            } else {
+                sum
+            }
+        })
+}
+
 fn part2(contents: String) -> isize {
-    0
+    let lines = contents.split('\n').take_while(|x| !x.is_empty());
+    let height = contents.matches('\n').count();
+    let width = contents.find('\n').unwrap();
+    let mut map =
+        Array2D::from_iter_row_major(lines.flat_map(|x| x.chars()), height, width).unwrap();
+
+    let mut seen_states: HashMap<Array2D<char>, usize> = HashMap::new();
+    let mut i = 1000000000;
+    while i > 0 {
+        if let Some(&last_seen_time) = seen_states.get(&map) {
+            let cycle_time = last_seen_time - i;
+            i %= cycle_time;
+            if i == 0 {
+                break;
+            }
+        }
+        seen_states.insert(map.clone(), i);
+        spin_cycle(&mut map);
+        i -= 1;
+    }
+    get_north_load(&map) as isize
 }
 
 #[cfg(test)]
@@ -80,6 +181,16 @@ mod tests {
             input_file: "sample.txt",
             part_num: 1,
             expected_out: 136,
+        }
+        .run()
+    }
+
+    #[test]
+    fn first_sample_part_two() {
+        Sample {
+            input_file: "sample.txt",
+            part_num: 2,
+            expected_out: 64,
         }
         .run()
     }
